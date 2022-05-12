56f30c8a8fd880e559d4a91aee5583f7329d7c70
nkizito92 <nkizito57@gmail.com>
2022-05-12 18:32:10 -0500

    the app component has the routes set for routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import "./App.css"
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux"
 import { getCardsFetch } from './saga/reducers/cardReducer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navigation from "./components/navComponents/Navigation"
+import Cards from "./components/cardComponents/Cards"
+import CardForm from "./components/cardComponents/CardForm"
+import CardShow from "./components/cardComponents/CardShow"
+import CardEdit from "./components/cardComponents/CardEdit"
 
-function App() {
+const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getCardsFetch())
   }, [dispatch])
-  const cards = useSelector(state => state.reducer.cards)
-  console.log(cards)
+  const cards = useSelector(state => state.show.cards)
   return (
     <div className="App">
-      Hi
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route exact path="/" element={<Cards cards={cards}/>} />
+          <Route exact path="/createCard" element={<CardForm  />} />
+          <Route exact path="/cards/:id" element={<CardShow cards={cards}/>} />
+          <Route exact path="/cards/:id/edit" element={<CardEdit  />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
