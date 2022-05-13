c4e2b97306d4eb55b75dbd1fe23d6ef6f1614928
nkizito92 <nkizito57@gmail.com>
2022-05-12 21:48:27 -0500

    added function actions newCardPost, updateCardSuccess, deleteCardSuccess

--- src/saga/reducers/cardReducer.js
+++ src/saga/reducers/cardReducer.js
@@ -1,25 +1,51 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 export const cardReducer = createSlice({
-    name: 'cards', 
-        initialState: {
-            cards: [],
-            isLoading: false
+    name: 'cards',
+    initialState: {
+        cards: [],
+        isLoading: false
+    },
+    reducers: {
+        getCardsFetch: (state) => {
+            state.isLoading = true;
         },
-        reducers: {
-            getCardsFetch: (state) => {
-                state.isLoading = true;
-            },
-            getCardsSuccess: (state, action) => {
-                state.cards = action.payload;
-                state.isLoading = false
-            },
-            getCardsFailure: (state) => {
-                state.isLoading = false;
+        getCardsSuccess: (state, action) => {
+            state.cards = action.payload;
+            state.isLoading = false
+        },
+        newCardPost: (state, action) => {
+            return {
+                ...state, cards: [...state.cards, action.payload]
             }
+        },
+        updateCardSuccess: (state, action) => {
+            const idx = state.cards.findIndex(card => card.id === Number(action.payload.id))
+            const newState = [...state.cards]
+            newState.splice(idx, 1, action.payload)
+            return {
+                ...state, cards: [...newState]
+            }
+        },
+        deleteCardSuccess: (state, action) => {
+            return {
+                ...state, 
+                cards: [...state.cards.filter(card => card.id !== Number(action.payload.id))]
+            }
+        },
+        getCardsFailure: (state) => {
+            state.isLoading = false;
         }
-    
+    }
+
 });
 
-export const { getCardsFetch, getCardsSuccess, getCardsFailure} = cardReducer.actions;
+export const { 
+    getCardsFetch, 
+    getCardsSuccess, 
+    newCardPost, 
+    updateCardSuccess, 
+    deleteCardSuccess, 
+    getCardsFailure 
+} = cardReducer.actions;
 
 export default cardReducer.reducer;
