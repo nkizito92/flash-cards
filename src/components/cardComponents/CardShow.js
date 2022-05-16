d6a1950c8d0630448383758ee7466941a3219b49
nkizito92 <nkizito57@gmail.com>
2022-05-15 23:46:55 -0500

    created a test for more trues than falses

--- src/components/cardComponents/CardShow.js
+++ src/components/cardComponents/CardShow.js
@@ -1,17 +1,34 @@
 import { useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 const CardShow = ({ cards }) => {
     const params = useParams()
     const card = cards.find(card => card.id === Number(params.id))
     const [cardDef, setCardDef] = useState("")
+    const [simAns, setSimAns] = useState("")
     const [answer, setAnswer] = useState("")
-    const cardDefinition = (definition) => {
-        // take def from card and compare it to user version
+    const cardDefinition = (definition= "You are displaying the definition") => {
+        // take definition from card and compare it to user own defintion
         // click to go to next and back as well
-        const theAnswer = "You are displaying the definition"
-        theAnswer.split("")
-        if (theAnswer.includes(answer)) {
+        const theAnswer = definition.toLocaleLowerCase().split(" ")
+        const isAnswer = () => {
+            const results = theAnswer.map(word => answer.split(" ").includes(word))
+            const common = {
+                true: 0,
+                false: 0
+            }
+            for (let i of results) {
+                if (common[i] > 0) {
+                    common[i]++
+                } else {
+                    common[i] = 1
+                }
+            }
+            console.log(common)
+            return common.true > common.false
+        }
+        if (!!isAnswer()) {
             setCardDef("You are displaying the definition")
+            setSimAns(answer)
         } else {
             setCardDef("That is not correct")
         }
@@ -19,10 +36,13 @@ const CardShow = ({ cards }) => {
     return (
         <div>
             <h2> This is CardShow</h2>
-            {card && <h3>{card.full_name}</h3>}
-            <div>{cardDef}</div>
+            <h3>{card.full_name}</h3>
+            {answer && <div>{cardDef}</div>}
+            {simAns && <div>Your answer: {answer} ✅</div>}
             <input type="text" onChange={e => setAnswer(e.target.value)} value={answer} />
             <button onClick={() => cardDefinition()}>View Definition</button>
+            <Link to={`/cards/${params.id}/edit`} >Edit Card</Link>
+            {cardDef && <div><Link onClick={()=>setCardDef("")} to={`/cards/${Math.floor(Math.random()*41)+1}`}> Next</Link></div>}
         </div>
     )
 }
