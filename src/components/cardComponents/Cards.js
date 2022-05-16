e6a8ecbea23c02904c9289fc389427119a9229df
nkizito92 <nkizito57@gmail.com>
2022-05-15 23:47:42 -0500

    shows a link for each card

--- src/components/cardComponents/Cards.js
+++ src/components/cardComponents/Cards.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { Link } from 'react-router-dom'
 const Cards = ({ cards }) => {
-    const cardsList = cards.map(card =>
+    const cardsList = cards && cards.map(card =>
         <div key={`cardsKeing-${card.id}`}><Link to={`/cards/${card.id}`}>View {card.full_name}</Link></div>)
     return (
         <div>
