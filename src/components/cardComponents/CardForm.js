3e7ab0df3f1f494bbd20947a7fdbfd714eb37386
nkizito92 <nkizito57@gmail.com>
2022-05-17 22:05:27 -0500

    card form to create a card

--- src/components/cardComponents/CardForm.js
+++ src/components/cardComponents/CardForm.js
@@ -1,15 +1,42 @@
-import {useState, useEffect} from "react"
-
+import {useState} from "react"
+import { useDispatch, connect } from "react-redux"
+import { newCardPost } from "../../saga/reducers/cardReducer"
 const CardForm = () => {
+    const dispatch = useDispatch()
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState("")
+    const [id, setId] = useState("")
+    function handleSubmit(e) {
+        e.preventDefault()
+        const newClient = {
+            full_name : name,
+            pay_rate: price,
+            driver_id: id
+        }
+        dispatch(newCardPost(newClient))
+    }
     return (
         <div>
             This is CardsForm 
             <form >
+                <div>
+                <input type="text" name="driver_id"
+                    onChange= {e=> setId(e.target.value)} value={id} placeholder="id"/>
 
-                <input type="text" />
-                <button>Create Card</button>
+                </div>
+                <div>
+                <input type="text" name="full_name"
+                     onChange={(e)=>setName(e.target.value)} value={name}   placeholder="full_name"/>
+
+                </div>
+                <div>
+                <input type="text" name="pay_rate"
+                     onChange={e =>setPrice(e.target.value)} value={price}  placeholder="pay_rate" />
+
+                </div>
+                <button onClick={e=>handleSubmit(e)}>Create Card</button>
             </form>
         </div>
     )
 }
-export default CardForm
+export default connect(null,{newCardPost})(CardForm)
