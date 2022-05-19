8e9dfa6fcbebb7ffc447762c2d78e232ecefb0d3
nkizito92 <nkizito57@gmail.com>
2022-05-18 21:50:00 -0500

    all saga calls are set and working

--- src/saga/requests/cardsSaga.js
+++ src/saga/requests/cardsSaga.js
@@ -1,9 +1,9 @@
-import {call, put, takeEvery} from 'redux-saga/effects'
+import { call, put, takeEvery, select } from 'redux-saga/effects'
 import { getCardsSuccess } from '../reducers/cardReducer'
 import axios from 'axios';
 
 function* workGetCardsFetch() {
-    const cards = yield call( () => fetch('http://localhost:3001/clients'))
+    const cards = yield call(() => fetch('http://localhost:3001/clients'))
     const formattedCards = yield cards.json();
     yield put(getCardsSuccess(formattedCards))
 }
@@ -12,8 +12,8 @@ function* workCreateCard(action) {
     try {
 
         const client = action.payload
-        yield call( ()=> axios.post('http://localhost:3001/clients', {client}))
-    } catch(error){
+        yield call(() => axios.post('http://localhost:3001/clients', { client }))
+    } catch (error) {
         console.log(error.message)
     }
 }
@@ -21,7 +21,10 @@ function* workCreateCard(action) {
 function* updateCard(action) {
     const client = action.payload
     try {
-        yield call (() => axios.patch(`http://localhost:3001/clients/${client.id}`, {client}))
+        yield call(() => axios.patch(`http://localhost:3001/clients/${client.id}`, { client }))
+        const clients = yield select(state => state.show.cards)
+        const testingGetCards = getCardsSuccess(clients)
+        yield put(testingGetCards)
     } catch (error) {
         console.log(error.message)
     }
@@ -30,12 +33,12 @@ function* updateCard(action) {
 function* deleteCard(action) {
     const client = action.payload
     try {
-        yield call (() => axios.delete(`http://localhost:3001/clients/${client.id}`, {client}))
+        yield call(() => axios.delete(`http://localhost:3001/clients/${client.id}`, { client }))
     } catch (error) {
         console.log(error.message)
     }
 }
-function* cardsSaga(){
+function* cardsSaga() {
     yield takeEvery('cards/getCardsFetch', workGetCardsFetch)
     yield takeEvery('cards/newCardPost', workCreateCard)
     yield takeEvery('cards/updateCardSuccess', updateCard)
