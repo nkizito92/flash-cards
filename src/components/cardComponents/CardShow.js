2cca78e2d71ae29cf0a59e7469c4f909f1aa8522
nkizito92 <nkizito57@gmail.com>
2022-05-22 21:16:20 -0500

    added isPassing state

--- src/components/cardComponents/CardShow.js
+++ src/components/cardComponents/CardShow.js
@@ -1,17 +1,18 @@
-import { useState } from "react"
-import { useParams, Link} from "react-router-dom"
+import React, { useState } from "react"
+import { useParams, Link } from "react-router-dom"
 const CardShow = ({ cards }) => {
     const params = useParams()
     const card = cards.find(card => card.id === Number(params.id))
     const [cardDef, setCardDef] = useState("")
     const [simAns, setSimAns] = useState("")
     const [answer, setAnswer] = useState("")
-    const cardDefinition = (definition= "You are displaying the definition") => {
+    const [isPass, setIsPass] = useState("")
+    const cardDefinition = (definition = "You are displaying the definition") => {
         // take definition from card and compare it to user own defintion
         // click to go to next and back as well
-        const theAnswer = definition.toLocaleLowerCase().split(" ")
+        const theAnswer = definition.toLowerCase().split(" ")
         const isAnswer = () => {
-            const results = theAnswer.map(word => answer.split(" ").includes(word))
+            const results = theAnswer.map(word => answer.toLowerCase().split(" ").includes(word))
             const common = {
                 true: 0,
                 false: 0
@@ -24,6 +25,7 @@ const CardShow = ({ cards }) => {
                 }
             }
             console.log(common)
+            setIsPass(common.true > common.false)
             return common.true > common.false
         }
         if (!!isAnswer()) {
@@ -33,20 +35,20 @@ const CardShow = ({ cards }) => {
             setCardDef("That is not correct")
         }
     }
-    if(card){
+    if (card) {
         return (
             <div>
-            <h2> This is CardShow</h2>
-            <h3>{card.full_name}</h3>
-            {answer && <div>{cardDef}</div>}
-            {simAns && <div>Your answer: {answer} ✅</div>}
-            <input type="text" onChange={e => setAnswer(e.target.value)} value={answer} />
-            <button onClick={() => cardDefinition()}>View Definition</button>
-            <Link to={`/cards/${params.id}/edit`} >Edit Card</Link>
-            {cardDef && <div><Link onClick={()=>setCardDef("")} to={`/cards/${Math.floor(Math.random()*41)+1}`}> Next</Link></div>}
-        </div>
-    )
-}
+                <h2> This is CardShow</h2>
+                <h3>{card.full_name}</h3>
+                {answer && <div>{cardDef}</div>}
+                {isPass && <div>Your answer: {answer} ✅</div>}
+                <input type="text" onChange={e => setAnswer(e.target.value)} value={answer} />
+                <button onClick={() => cardDefinition()}>View Definition</button>
+                <Link to={`/cards/${params.id}/edit`} >Edit Card</Link>
+                {isPass && <div><Link onClick={() => setCardDef("")} to={`/cards/${Math.floor(Math.random() * 41) + 1}`}> Next</Link></div>}
+            </div>
+        )
+    }
 }
 
 export default CardShow
