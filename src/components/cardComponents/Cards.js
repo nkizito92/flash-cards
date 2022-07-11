21c60e7a7f8399c16b978e3920dad6d870b7b535
Kizito Njoku <nkizito57@gmail.com>
2022-07-10 22:24:12 -0500

    Added map to display multiple links to components

--- src/components/cardComponents/Cards.js
+++ src/components/cardComponents/Cards.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-const Cards = ({ cards }) => {
+const Cards = ({cards}) => {
     const cardsList = cards && cards.map(card =>
-        <div key={`cardsKeing-${card.id}`}><Link to={`/cards/${card.id}`}>View {card.full_name}</Link></div>)
+        <div key={`cardsKeing-${card.id}`}>
+            <Link to={`/cards/${card.id}/`}>View {card.name}</Link>
+        </div>)
     return (
         <div>
            <h1>Select a Card</h1> 
