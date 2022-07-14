be68614f6e176b28300ef7fe7c247e6f57be5ed9
Kizito Njoku <nkizito57@gmail.com>
2022-07-13 23:58:13 -0500

    added usecontext for contextData to display the username

--- src/components/navComponents/Navigation.js
+++ src/components/navComponents/Navigation.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {useContext} from 'react'
+import { NavLink, Link} from 'react-router-dom'
+import AuthContext from '../adminComponents/AuthContext'
 import "./nav.css"
 const Navigation = () => {
+    const {contextData} = useContext(AuthContext)
+    const user = contextData.user
     const active = (isActive) => {
         return {
             backgroundColor: isActive ? "white" : "",
@@ -10,8 +13,12 @@ const Navigation = () => {
     }
     return (
         <div className='nav'>
-            <NavLink to="/" style={({ isActive }) => active(isActive)}>View Cards</NavLink>
-            <NavLink to="/createCard" style={({ isActive }) => active(isActive)} >Create Card</NavLink>
+            <NavLink to="/cards" style={({ isActive }) => active(isActive)}>View Cards</NavLink>
+            <NavLink to="/cards/new/" style={({ isActive }) => active(isActive)} >Create Card</NavLink>
+           {user ? (
+                <button onClick={contextData.logout}>Logout</button>
+           ): <Link to='/login'>Login</Link>}
+           {user && <div style={{color: "white"}}>{user.username}</div> }
         </div>
     )
 }
