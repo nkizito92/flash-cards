ff604a9c0391cd35de21ad17d9efe5178ad3f82e
Kizito Njoku <nkizito57@gmail.com>
2022-08-07 02:26:40 -0500

    change navLink to the new cards link and fixed active issue

--- src/components/navComponents/Navigation.js
+++ src/components/navComponents/Navigation.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
-import { NavLink, Link} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 import AuthContext from '../adminComponents/AuthContext'
-import "./nav.css"
+import "./nav.scss"
 const Navigation = () => {
     const {contextData} = useContext(AuthContext)
     const user = contextData.user
@@ -13,12 +13,14 @@ const Navigation = () => {
     }
     return (
         <div className='nav'>
+            <NavLink to="/" style={({ isActive }) => active(isActive)}>Home</NavLink>
             <NavLink to="/cards" style={({ isActive }) => active(isActive)}>View Cards</NavLink>
-            <NavLink to="/cards/new/" style={({ isActive }) => active(isActive)} >Create Card</NavLink>
-           {user ? (
-                <button onClick={contextData.logout}>Logout</button>
-           ): <Link to='/login'>Login</Link>}
-           {user && <div style={{color: "white"}}>{user.username}</div> }
+            <NavLink to="/new/card" style={({ isActive }) => active(isActive)} >Create Card</NavLink>
+            <div className='userName'>{user ? (
+                    <NavLink to='/login' onClick={contextData.logout}>Logout</NavLink>
+            ): <NavLink to='/login' style={({ isActive }) => active(isActive)}>Login</NavLink>}
+            {user && <div style={{color: "white", padding: "10px"}}>{user.username}</div> }
+            </div>
         </div>
     )
 }
